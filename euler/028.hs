f0e38318291300e88e6df6ac9e9a790c4827a226
Willem Melching <willem.melching@gmail.com>
2014-07-15 22:24:46 +0100

    euler 028

---
+++ euler/028.hs
@@ -0,0 +1,20 @@
+{-
+Problem 28: Number spiral diagonals
+
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20 07 08 09 10
+19 06 01 02 11
+18 05 04 03 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+-}
+
+sumDiag :: Integer -> Integer
+sumDiag k =  sum [4 * (2*n + 1)^2 - 12 * n | n <- [1..k]] + 1
+
+main = putStrLn $ show $ sumDiag (1001 `quot` 2)

2b51f4e9e44c964bb5d8f2b4eac0ddd9cc6d65fc
Willem Melching <willem.melching@gmail.com>
2017-12-14 12:32:02 -0800

    optimize 13 for speed

--- advent_of_code_2017/day_13/01.py
+++ advent_of_code_2017/day_13/01.py
@@ -6,7 +6,13 @@ print sum([depth * r if depth % (2 * r - 2) == 0 else 0 for (depth, r) in scanne
 
 delay = 0
 while True:
-    if not any([(delay + depth) % (2 * r - 2) == 0 for (depth, r) in scanners]):
+    found = True
+    for depth, r in scanners:
+        if (delay + depth) % (2 * r - 2) == 0:
+            found = False
+            break
+
+    if found:
         print delay
         break
 

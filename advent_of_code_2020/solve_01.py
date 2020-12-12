da672efae5dc939e519a2bb427d8a02ec04002f4
Willem Melching <willem.melching@gmail.com>
2020-12-12 12:15:06 +0100

    cleanup day 1

--- advent_of_code_2020/solve_01.py
+++ advent_of_code_2020/solve_01.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
+import math
+import itertools
 
-def solve_01(numbers):
-    for x in numbers:
-        for y in numbers:
-            if x + y == 2020:
-                return x * y
-
-def solve_02(numbers):
-    for x in numbers:
-        for y in numbers:
-            for z in numbers:
-                if x + y + z == 2020:
-                    return x * y * z
+def solve(numbers, repeat=2):
+    for s in itertools.product(numbers, repeat=repeat):
+        if sum(s) == 2020:
+            return math.prod(s)
 
 numbers = []
 with open('input_01') as f:
     for line in f:
         numbers.append(int(line))
 
-print(solve_01(numbers))
-print(solve_02(numbers))
-
+print(solve(numbers, 2))
+print(solve(numbers, 3))

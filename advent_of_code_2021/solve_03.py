c9b92350ad50096cb994431ac91401a6691764a1
Willem Melching <willem.melching@gmail.com>
2021-12-03 09:42:47 +0100

    day 3 refactor

--- advent_of_code_2021/solve_03.py
+++ advent_of_code_2021/solve_03.py
@@ -2,17 +2,20 @@
 
 import numpy as np
 
+def one_most_common(nums, n):
+    column = nums[:, n]
+    ratio = np.sum(column) / nums.shape[0]
+    return ratio >= 0.5
+
 def filter_list(inputs, upper=True):
     N = inputs.shape[1]
     nums = inputs.copy()
 
     for n in range(N):
-        column = nums[:, n]
-        ratio = np.sum(column) / nums.shape[0]
-        if bool(ratio >= 0.5) == upper:
-            nums = nums[column > 0.5]
+        if one_most_common(nums, n) == upper:
+            nums = nums[nums[:, n] > 0.5]
         else:
-            nums = nums[column < 0.5]
+            nums = nums[nums[:, n] < 0.5]
 
         if len(nums) == 1:
             break
@@ -21,17 +24,17 @@ def filter_list(inputs, upper=True):
     return int(x, 2)
 
 
-inputs = np.asarray([list(map(int, list(n.strip()))) for n in open("input_03")])
-N = inputs.shape[1]
+if __name__ == "__main__":
+    inputs = np.asarray([list(map(int, list(n.strip()))) for n in open("input_03")])
+    N = inputs.shape[1]
 
-gamma = 0
-epsilon = 0
-for n in range(N):
-    ratio = np.sum(inputs[:, N - n - 1]) / inputs.shape[0]
-    if ratio > 0.5:
-        gamma += 2**n
-    else:
-        epsilon += 2**n
-print(gamma * epsilon)
+    # Part one
+    gamma = 0
+    for n in range(N):
+        if one_most_common(inputs, N - n - 1):
+            gamma += 2**n
+    epsilon = gamma ^ (2**N - 1)
+    print(gamma * epsilon)
 
-print(filter_list(inputs) * filter_list(inputs, False))
+    # Part two
+    print(filter_list(inputs) * filter_list(inputs, False))

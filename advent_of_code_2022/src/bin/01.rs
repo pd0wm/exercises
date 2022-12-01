24c9af2aab0d738d54b060d7f4b939b7a2a73157
Willem Melching <willem.melching@gmail.com>
2022-12-01 13:06:37 +0100

    day 1: benchmarking and faster part 2

--- advent_of_code_2022/src/bin/01.rs
+++ advent_of_code_2022/src/bin/01.rs
@@ -1,27 +1,37 @@
+#![feature(test)]
+#![feature(binary_heap_into_iter_sorted)]
+
+extern crate test;
+
+use std::collections::BinaryHeap;
+
 fn part1(input: &Vec<Vec<u64>>) -> u64 {
     input.iter().map(|elf| elf.iter().sum()).max().unwrap()
 }
 
 fn part2(input: &Vec<Vec<u64>>) -> u64 {
-    let mut sums: Vec<u64> = input.iter().map(|elf| elf.iter().sum()).collect();
-    sums.sort();
-    sums[sums.len() - 3..].iter().sum()
+    let sums: BinaryHeap<u64> = input.iter().map(|elf| elf.iter().sum()).collect();
+    sums.into_iter_sorted().take(3).sum()
+}
+
+fn parse_input() -> Vec<Vec<u64>> {
+    let input = include_str!("../../inputs/01.txt").trim();
+    input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|value| value.parse().unwrap()).collect())
+        .collect()
 }
 
 fn main() {
-    let input = include_str!("../../inputs/01.txt").trim();
-    let values: Vec<Vec<u64>> = input
-        .split("\n\n")
-        .map(|elf| elf.lines().map(|value| value.parse().unwrap()).collect())
-        .collect();
-
+    let values = parse_input();
     println!("{}", part1(&values));
     println!("{}", part2(&values));
 }
 
 #[cfg(test)]
-mod tests {
+mod day01_tests {
     use super::*;
+    use test::{Bencher, black_box};
 
     #[test]
     fn example() {
@@ -34,5 +44,22 @@ mod tests {
         ];
         assert_eq!(24000, part1(&sample_input));
         assert_eq!(45000, part2(&sample_input));
+    }
+
+    #[bench]
+    fn bench_parsing(b: &mut Bencher) {
+        b.iter(|| parse_input());
+    }
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let values = black_box(parse_input());
+        b.iter(|| part1(&values));
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let values = black_box(parse_input());
+        b.iter(|| part2(&values));
     }
 }
